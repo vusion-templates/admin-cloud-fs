67f88151313b66ee85d89558f9879538e684ce99
Rainfore <rainforest92@126.com>
2019-04-15 15:45:45 +0800

    :bug: Fix <u-form-table>

--- client/components/index.js
+++ client/components/index.js
@@ -1,24 +1,18 @@
 import ULogo from './common/u-logo.vue';
-// import NavbarItem from './cloud-ui/u-navbar-item.vue';
 export * from './cloud-ui/u-panel.vue';
+
 import UHeadCard from './cloud-ui/u-head-card.vue';
 import UStatusIcon from './cloud-ui/u-status-icon.vue';
-// import MonitorChartPanel from './cloud-ui/u-monitor-chart-panel.vue';
-// import MonitorOptionbar from '@necfe/cloud-ui-internal/src/u-monitor-optionbar.vue';
-// import DateCustomPicker from '@necfe/cloud-ui-internal/src/u-date-custom-picker.vue';
 import USubmitButton from './cloud-ui/u-submit-button.vue';
 import UConfirm from './cloud-ui/u-confirm.vue';
-import UFormTable from '@necfe/cloud-ui-internal/src/u-form-table.vue';
+import UFormTable from './cloud-ui/u-form-table.vue';
 
 export * from 'cloud-ui.vusion';
+
 export {
     ULogo,
-    // NavbarItem,
     UHeadCard,
     UStatusIcon,
-    // MonitorChartPanel,
-    // MonitorOptionbar,
-    // DateCustomPicker,
     USubmitButton,
     UConfirm,
     UFormTable,
